bbb366aa6b9fb20dfa0228a49c2f71080cb7b853
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2024-07-16 15:55:05 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var scriptOneTrust = document.createElement("script");
+scriptOneTrust.src =
+  "https://cdn.cookielaw.org/consent/9c2762b8-cb60-49d7-a1da-54467f8558c2/otSDKStub.js";
+scriptOneTrust.type = "text/javascript";
+scriptOneTrust.setAttribute("data-domain-script", "9c2762b8-cb60-49d7-a1da-54467f8558c2");
+document.head.appendChild(scriptOneTrust);
